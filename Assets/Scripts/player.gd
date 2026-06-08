32b2a159f42c5322cd344a4cf3f8142172bf6ea3
Brandon Manuel <brandon.t.manuel@gmail.com>
2026-06-08 16:58:11 -0500

    Added basic pickup animation

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -7,6 +7,7 @@ const RUN_SPEED = 75.0
 const JUMP_VELOCITY = -200.0
 
 var is_running: bool = false
+var allow_movement: bool = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,12 +18,18 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
+	# Handle pickup.
+	if Input.is_action_just_pressed("pick_up") and is_on_floor():
+		allow_movement = false
+		velocity.x = move_toward(velocity.x, 0, RUN_SPEED if is_running else WALK_SPEED)
+		animated_sprite_2d.play("pick_up")
+		
 	is_running = Input.is_action_pressed("run")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("walk_left", "walk_right")
-	if direction:
+	if allow_movement and direction:
 		if is_running:
 			animated_sprite_2d.play('run')
 			velocity.x = direction * RUN_SPEED
@@ -33,9 +40,14 @@ func _physics_process(delta: float) -> void:
 			animated_sprite_2d.flip_h = true
 		else:
 			animated_sprite_2d.flip_h = false
-	else:
+	elif not animated_sprite_2d.is_playing() or animated_sprite_2d.animation != 'pick_up':
 		animated_sprite_2d.play('idle')
 		velocity.x = move_toward(velocity.x, 0, RUN_SPEED if is_running else WALK_SPEED)
 
 
 	move_and_slide()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite_2d.animation == 'pick_up':
+		allow_movement = true
