382314e18849eedb50aa88fc96fafeb476b00a08
Brandon Manuel <brandon.t.manuel@gmail.com>
2026-06-10 14:48:21 -0500

    Carrot now waits for a half second before disappearing

--- Assets/Scripts/carrot.gd
+++ Assets/Scripts/carrot.gd
@@ -1,4 +1,5 @@
 extends Area2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 var tile_position
 func _ready():
@@ -6,6 +7,15 @@ func _ready():
 	tile_position = tilemap.local_to_map(position)
 	
 func picked_up():
+	var tween = get_tree().create_tween()
+	tween.tween_property(sprite_2d,"position", Vector2.UP * 5, .5).as_relative().set_trans(Tween.TRANS_LINEAR)
+	tween.parallel().tween_property(sprite_2d,"region_rect", Rect2(0, 0, 16.0, 32.0), .5).set_trans(Tween.TRANS_LINEAR)
+
+	tween.tween_callback(clear)
+
+
+func clear():
+	await get_tree().create_timer(.5).timeout
 	var tilemap = get_parent()
 	tilemap.set_cell(tile_position)
 	queue_free()
