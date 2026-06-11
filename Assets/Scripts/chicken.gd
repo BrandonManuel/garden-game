f5e3b0ded4d8eaab5931bc037f3d4cc1e7a01760
Brandon Manuel <brandon.t.manuel@gmail.com>
2026-06-11 12:55:13 -0500

    Chicken and bunny now extend from a walking_animal scene. Chicken has idle, peck, and walk animations

--- Assets/Scripts/chicken.gd
+++ Assets/Scripts/chicken.gd
@@ -1,25 +1,19 @@
-extends AnimatedSprite2D
+extends "res://Assets/Scripts/walking_animal.gd"
 
+const MIN_PECK_SECONDS: int = 1
+const MAX_PECK_SECONDS: int = 2
 
-const MIN_SECONDS: int = 3
-const MAX_SECONDS: int = 5
-
-var is_waiting: bool = false
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	super._ready()
+	animation_changed.connect(_on_animation_changed)
 
+func _on_animation_changed() -> void:
+	if animation == 'idle':
+		start_peck_loop()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if not is_waiting:
-		var random_float = randf_range(MIN_SECONDS, MAX_SECONDS) 
-		is_waiting = true
-		await get_tree().create_timer(random_float).timeout
+func start_peck_loop() -> void:
+	await get_tree().create_timer(randf_range(MIN_PECK_SECONDS, MAX_PECK_SECONDS)).timeout
+	if animation == 'idle':
 		self.play('peck')
-
-		
-	if not self.is_playing():
+		await animation_finished
 		self.play('idle')
-		is_waiting = false
---
+++ Assets/Scripts/walking_animal.gd
@@ -0,0 +1,48 @@
+extends AnimatedSprite2D
+
+@onready var left_detector: RayCast2D = $LeftDetector
+@onready var right_detector: RayCast2D = $RightDetector
+
+const SPEED = 25.0
+const MIN_SECONDS: int = 3
+const MAX_SECONDS: int = 5
+
+enum Direction {LEFT = -1, RIGHT = 1}
+
+var current_direction: Direction = Direction.RIGHT
+var is_walking: bool = false
+
+func _ready() -> void:
+	self.play('idle')
+
+func _process(delta: float) -> void:
+	if ((current_direction == Direction.LEFT and not left_detector.is_colliding()) or
+		(current_direction == Direction.RIGHT and not right_detector.is_colliding())):
+		turn_around()
+	if not is_walking:
+		start_roam_timer(delta)
+	elif animation == 'walk':
+		position.x += current_direction * SPEED * delta
+
+func start_roam_timer(delta: float) -> void:
+	is_walking = true
+	self.play('walk')
+	var random_float = randf_range(MIN_SECONDS, MAX_SECONDS)
+	await get_tree().create_timer(random_float).timeout
+	start_idle_timer()
+
+func start_idle_timer() -> void:
+	self.play('idle')
+	var random_float = randf_range(MIN_SECONDS, MAX_SECONDS)
+	await get_tree().create_timer(random_float).timeout
+	if randi_range(0, 1) == 0:
+		turn_around()
+	is_walking = false
+
+func turn_around() -> void:
+	if current_direction == Direction.LEFT:
+		current_direction = Direction.RIGHT
+		flip_h = false
+	else:
+		current_direction = Direction.LEFT
+		flip_h = true
