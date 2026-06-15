ee43c4a4d85836001b781e06024b9124af9dd977
Brandon Manuel <brandon-manuel@live.com>
2026-06-14 21:42:22 -0500

    Added new onion garden box scene and tile setup

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -27,7 +27,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not is_picking_up:
 		velocity.y = JUMP_VELOCITY
 		
 	# Handle pickup.
